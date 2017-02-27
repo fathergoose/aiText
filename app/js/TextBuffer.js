e0357f1319b00d70c25558a4347a2b3e0afce01c
Al Ilseman <al@ilseman.com>
2017-02-27 14:40:19 -0600

    tring to wire it up

--- app/js/TextBuffer.js
+++ app/js/TextBuffer.js
@@ -1,26 +1,27 @@
+var Caret = require('./DOM/caret');
+
 function TextBuffer() {
     var caretPosition = 0;
     var currentLine = 0;
     var lines = [''];
     return {
 
-        line: function() {
-          return lines[ currentLine];
+        line: function () {
+            return lines[currentLine];
         },
 
         // add a single character at current caret position and incriment caret
-        putChar: function(char) {
+        putChar: function (char) {
             lines[currentLine] = lines[currentLine].slice(0, caretPosition) + char + lines[currentLine].slice(caretPosition);
             caretPosition++;
-            console.log(this);
         },
 
         // insert new empty line below current line
-        newLine: function() {
+        newLine: function () {
             var newLine = document.createElement('div');
             newLine.innerHTML = '';
             newLine.setAttribute('class', 'line');
-            newLine.addEventListener("click", updateCaretByClick, false)
+            newLine.addEventListener("click", Caret.updateCaretByClick, false)
             main.appendChild(newLine);
             lines[currentLine + 1] = lines[currentLine].slice(caretPosition, -1);
             lines[currentLine] = lines[currentLine].slice(0, caretPosition);
@@ -28,17 +29,25 @@ function TextBuffer() {
             caretPosition = 0;
         },
 
-        deleteBackwards: function(int) {
+        deleteBackwards: function (int) {
             lines[currentLine] = lines[currentLine].slice(0, caretPosition - int) + lines[currentLine].slice(caretPosition)
             caretPosition -= int;
         },
 
-        getLines: function(begining, ending) {
+        /**
+         * Retrieves lines a.k.a content from the TextBuffer object 
+         * 
+         * @param {(number|number[])} begining 
+         * @param {number} ending 
+         * @returns {Object|Array} Object[lineNumber] = String for specific lists or range requests
+         * when no arguments are passed an array of all lines is returned 
+         */
+        getLines: function (begining, ending) {
             if (arguments.length === 0) {
-                return lines;
+                return lines
             } else if (Array.isArray(begining)) {
                 let returningLines = {};
-                begining.forEach( lineNumber => returningLines[lineNumber] = lines[lineNumber] )
+                begining.forEach(lineNumber => returningLines[lineNumber] = lines[lineNumber])
                 return returningLines;
             } else if (begining && ending) {
                 let returningLines = {};
@@ -49,6 +58,14 @@ function TextBuffer() {
             } else {
                 throw new Error('Inproper use of TextBuffer.getLines()')
             }
+        },
+
+        getCaretPosition: function () {
+            return caretPosition;
+        },
+
+        getCurrentLine: function () {
+            return currentLine;
         }
 
     }
--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-const observer = require('./SubjectObserver/observer');
+const Observer = require('./SubjectObserver/observer');
 const Subject = require('./SubjectObserver/subject');
 const TextBuffer = require('./TextBuffer');
 const extend = require('./extend');
@@ -7,9 +7,19 @@ var main = document.querySelector('#main');
 
 
 var textBuffer = new TextBuffer();
-extend(textBuffer, new Subject())
+extend(textBuffer, new Subject());
 
+var firstLine = document.createElement('div');
+firstLine.setAttribute('class', 'line');
+main.appendChild(firstLine);
+var linesOnScreen = document.getElementsByClassName('line');
 
+textBuffer.addObserver(linesOnScreen)
+linesOnScreen.update = function() {
+    textBuffer.getLines().forEach(function(line, i) {
+      linesOnScreen[i].innerText = line;
+    });
+};
 /* Insert Mode */
 window.addEventListener('keydown', function(event) {
     var T = textBuffer;
@@ -30,3 +40,36 @@ window.addEventListener('keydown', function(event) {
     }
 
 });
+
+
+
+/////////////////////
+
+var paragraphs = document.getElementsByClassName("line");
+for (i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].addEventListener("click", updateCaretByClick, false);
+}
+var caret = document.createElement('div');
+caret.setAttribute('id', 'caret');
+extend(caret, new Observer());
+textBuffer.addObserver(caret);
+
+caret.update = function() {
+    this.setAttribute('style', 'height: 100%; width: .6em; position: absolute; display: inline; top: 0; border-left: 1px; opacity: 1; animation: fade 1s ease-in-out infinite;');
+    var pos
+    if (linesOnScreen[textBuffer.getCurrentLine()].firstChild != null) {
+        pos = linesOnScreen[textBuffer.getCurrentLine()].firstChild.splitText(textBuffer.getCaretPosition());
+        linesOnScreen[textBuffer.getCurrentLine()].insertBefore(caret, pos)
+    } else {
+        console.log(linesOnScreen[textBuffer.getCurrentLine()]);
+        linesOnScreen[textBuffer.getCurrentLine()].appendChild(caret);
+    }
+};
+
+function updateCaretByClick(e) {
+    var range = document.caretRangeFromPoint(e.clientX, e.clientY);
+    var textNode = range.startContainer;
+    var offset = range.startOffset;
+    textBuffer.caretPosition = offset;
+    caret.update();
+}
