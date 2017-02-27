c0cfc0d4c8aea48441d33dbf19c98cc88b22af1f
Al.Ilseman <Al.Ilseman@AILSEMAN10CHI>
2017-02-27 12:40:23 -0600

    progress

--- app/js/main.js
+++ app/js/main.js
@@ -1,38 +1,32 @@
 const observer = require('./SubjectObserver/observer');
 const Subject = require('./SubjectObserver/subject');
+const TextBuffer = require('./TextBuffer');
 const extend = require('./extend');
 
 var main = document.querySelector('#main');
 
 
-/* Insert Mode */
+var textBuffer = new TextBuffer();
+extend(textBuffer, new Subject())
 
-window.addEventListener('keydown', function (event) {
-  switch (event.which) {
-    case 8: // backspace
-      main.innerText = main.innerText.slice(0, -1)
-      break;
-    case 16: // shift
-      // do nothing with shift
-      break;
-    default:
-      main.innerText += event.key;
-  }
+
+/* Insert Mode */
+window.addEventListener('keydown', function(event) {
+    var T = textBuffer;
+    switch (event.which) {
+        case 8: // backspace
+            T.deleteBackwards(1);
+            T.notify();
+            break;
+        case 13: // enter
+            T.newLine();
+            T.notify();
+            break;
+        case 16: // shift
+            break;
+        default:
+            T.putChar(event.key)
+            T.notify();
+    }
 
 });
-
-function TextBuffer() {
-    this.lines = [ 'hi al', 'boo index 1'];
-}
-
-TextBuffer.prototype.getLine = function (lineNumber) {
-    return this.lines[lineNumber];
-};
-
-TextBuffer.prototype.putLine = function (line) {
-    this.lines.push(line)
-    return this.lines.length;
-};
-
-
-window.appTextBuffer = TextBuffer;
